efb275545e220bbbb6de9aeb0c706c4bdbf76872
Ramkesh Sehara <ug201310026@iitj.ac.in>
2017-08-05 13:38:07 +0530

    Update success.php

--- success.php
+++ success.php
@@ -37,7 +37,7 @@ $mail->isSMTP();                            // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';             // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                     // Enable SMTP authentication
 $mail->Username = 'rksehara95@gmail.com';          // SMTP username
-$mail->Password = 'ramkesh@9672'; // SMTP password
+$mail->Password = ''; // SMTP password
 $mail->SMTPSecure = 'tls';                  // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                          // TCP port to connect to
 $mail->setFrom('rksehara95@gmail.com', 'CodexWorld');
